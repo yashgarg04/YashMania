b8dce5680e6460af38cd27149492e5cc02132789
Yash <yashgarg0408@gmail.com>
2023-09-20 11:19:27 +0530

    dark mode and remaining pages

--- src/app/blog/[blogid]/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const blogPage = () => {
-  return (
-    <div>blogPage</div>
-  )
-}
-
-export default blogPage
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
-// import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 // import { signOut, useSession } from "next-auth/react";
 
 const links = [
@@ -48,7 +48,7 @@ const Navbar = () => {
         YashMania
       </Link>
       <div className={styles.links}>
-        {/* <DarkModeToggle /> */}
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
